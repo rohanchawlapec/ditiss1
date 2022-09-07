648143ceb828a5592ea3fc04f1ab8ea7cba84f63
“rohanchawlapec” <rohanlove.chawla@gmail.com>
2022-09-07 19:38:15 +0530

    userinfo with shell info

--- bash_script.bash
+++ bash_script.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 _user="$(id -u -n)"
 _uid="$(id -u)"
+_shell_info="$(echo $SHELL)"
 echo "User name : $_user"
 echo "User name ID (UID) : $_uid"
+echo "MY shell name is  : $_shell_info"
